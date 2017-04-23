046cf99089a65d0a288226acf14d10538cfb14a5
Chris Smith <42@chrisryansmith.com>
2017-04-23 08:57:47 -0600

    add cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,8 +5,15 @@ require 'pry'
 class NumberGuesser
   attr_reader :secret_number
 
+  @@guesses_left = 5
+
   def initialize
     @secret_number = rand(100)
+  end
+
+  def guesses_left
+    return "Last Guess!" if @@guesses_left == 0
+    "You have #{@@guesses_left} guesses left."
   end
 
   def messages(guess)
@@ -14,14 +21,37 @@ class NumberGuesser
       no_guess: "Enter your guess:",
       way_too_high: "#{guess} is way too high!",
       too_high: "#{guess} is too high!",
-      just_right: "You got it right!<br/>The SECRET NUMBER is #{secret_number}.",
+      just_right: "You got it right!<br/>The SECRET NUMBER is #{secret_number}.<br/>A new number has been generated. Enter your guess:",
       way_too_low: "#{guess} is way too low!",
-      too_low: "#{guess} is too low!"
+      too_low: "#{guess} is too low!",
+      you_lost: "You ran out of guesses.  A new number has been generated. Enter your guess:"
     }
   end
 
+  def cheat(status)
+    if !status.nil?
+      "CHEAT MODE ENABLE:  The number is #{secret_number}"
+    end
+  end
+
+  def reset_game
+    @@guesses_left = 5
+    @secret_number = rand(100)
+  end
+
+  def you_win
+    reset_game
+    return :just_right
+  end
+
   def eval_message(guess)
+    if @@guesses_left == 0
+      reset_game
+      return :you_lost
+    end
+
     return :no_guess if guess.nil?
+    @@guesses_left -= 1
     diff = guess.to_i - secret_number
 
     is_correct      = ->(diff) { diff == 0 }
@@ -30,7 +60,7 @@ class NumberGuesser
     is_way_too_low  = ->(diff) { diff <= -5 }
 
     case diff
-    when is_correct then :just_right
+    when is_correct then you_win
     when is_way_too_high then :way_too_high
     when is_too_high then :too_high
     when is_way_too_low then :way_too_low
@@ -46,9 +76,13 @@ get '/' do
 
   result = ng.eval_message(guess)
 
+
+
   erb :index, :locals => {
     :number  => ng.secret_number,
     :message => ng.messages(guess)[result],
-    :classname   => result.to_s.gsub("_","-")
+    :classname   => result.to_s.gsub("_","-"),
+    :guesses_left => ng.guesses_left,
+    :cheat_mode => ng.cheat(params[:cheat])
   }
 end
