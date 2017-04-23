757a14426589c7bbfea808419e1386e35e0654b2
Chris Smith <42@chrisryansmith.com>
2017-04-22 20:32:46 -0600

    remove guard

--- app/web_guesser.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra'
-require 'sinatra/reloader'
-require 'rack-livereload'
-
-use Rack::LiveReload, :host => "localhost"
-
-get '/' do
-  "Hello, World!"
-end
---
+++ web_guesser.rb
@@ -0,0 +1,6 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  "The SECRET NUMBER is #{rand(100)}."
+end
