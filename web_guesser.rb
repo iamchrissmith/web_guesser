f93a54282b9d3765dd968f395aaceed30a7f4ab4
Chris Smith <42@chrisryansmith.com>
2017-04-23 08:13:30 -0600

    change backgrounds with guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,7 +6,8 @@ require 'pry'
 set :secret_number, rand(100)
 
 def eval_message(guess)
-  diff = guess - settings.secret_number
+  return :no_guess if guess.nil?
+  diff = guess.to_i - settings.secret_number
 
   is_correct      = ->(diff) { diff == 0 }
   is_way_too_high = ->(diff) { diff >= 5 }
@@ -14,21 +15,31 @@ def eval_message(guess)
   is_way_too_low  = ->(diff) { diff <= -5 }
 
   case diff
-  when is_correct then "You got it right!<br/>The SECRET NUMBER is #{settings.secret_number}."
-  when is_way_too_high then "Way Too High!"
-  when is_too_high then "Too high!"
-  when is_way_too_low then "Way Too Low!"
-  else "Too Low!"
+  when is_correct then :just_right
+  when is_way_too_high then :way_too_high
+  when is_too_high then :too_high
+  when is_way_too_low then :way_too_low
+  else :too_low
   end
 end
 
 get '/' do
-  guess = params[:guess].to_i
+  guess = params[:guess]
 
-  message = eval_message(guess)
+  messages = {
+    no_guess: "Enter your guess:",
+    way_too_high: "#{guess} is way too high!",
+    too_high: "#{guess} is too high!",
+    just_right: "You got it right!<br/>The SECRET NUMBER is #{settings.secret_number}.",
+    way_too_low: "#{guess} is way too low!",
+    too_low: "#{guess} is too low!"
+  }
+
+  result = eval_message(guess)
 
   erb :index, :locals => {
-    :number => settings.secret_number,
-    :message => message,
+    :number  => settings.secret_number,
+    :message => messages[result],
+    :classname   => result.to_s.gsub("_","-")
   }
 end
